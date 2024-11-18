85f21db350b38b1e5203429054d5e6a23db72ef8
glory <reallynotme.only@gmail.com>
2024-11-18 20:30:52 +0800

    cc

--- exts/Zbot/Zbot/tasks/moving/zbot6_direct/__init__.py
+++ exts/Zbot/Zbot/tasks/moving/zbot6_direct/__init__.py
@@ -7,7 +7,8 @@ from . import agents
 # from .zbot_env_v03 import ZbotSEnv, ZbotSEnvCfg
 # from .zbot_env_v04 import ZbotSEnv, ZbotSEnvCfg
 # from .zbot_env_v05 import ZbotSEnv, ZbotSEnvCfg
-from .zbot_env_v06 import ZbotSEnv, ZbotSEnvCfg
+# from .zbot_env_v06 import ZbotSEnv, ZbotSEnvCfg
+from .zbot_env_v07 import ZbotSEnv, ZbotSEnvCfg
 
 ##
 # Register Gym environments.
